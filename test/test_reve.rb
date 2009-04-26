274099255e37f5e1197535ca20640f1eace3a116
lisa <github@thedoh.com>
2009-04-25 20:53:56 -0400

    Let's see if importing svn r116 works.

--- test/test_reve.rb
+++ test/test_reve.rb
@@ -190,7 +190,7 @@ class TestReve < Test::Unit::TestCase
       assert_not_nil station.id
       assert_not_nil station.name
       assert_not_nil station.type_id
-      assert_not_nil station.type_name
+      assert_not_nil station.system_id
       assert_not_nil station.corporation_id
       assert_not_nil station.corporation_name
     end
@@ -917,6 +917,18 @@ class TestReve < Test::Unit::TestCase
       assert medal.is_public?
       assert ! medal.is_private?
     end
+  end
+  
+  def test_server_status
+    Reve::API.server_status_url = XML_BASE + 'server_status.xml'
+    status = nil
+    assert_nothing_raised do
+      status = @api.server_status
+    end
+    assert_kind_of(Reve::Classes::ServerStatus, status)
+    assert_equal(34444, status.players)
+    assert status.open?
+    assert status.open
   end
   
   def test_character_medals
