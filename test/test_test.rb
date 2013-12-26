4c346bd9ba5fd184fce758478fb06e23f754d5bd
Frances Morales <fmorales@francesmorales.com>
2013-12-26 14:58:00 -0800

    Dumbing things down even more

---
+++ test/test_test.rb
@@ -0,0 +1,17 @@
+require 'test/unit'
+require 'fileutils' # for saving downloaded XML
+$LOAD_PATH << './lib'
+require 'reve'
+
+
+class TestReve < Test::Unit::TestCase
+
+
+#This test verifies that we can connect to the CPP API Server. 
+#Dont care what data comes back, just as long as data comes back.
+  def test_End_to_End_Connectivity_Test
+    api = Reve::API.new
+    errors = api.errors 
+    assert_not_nil(errors.inspect)
+  end
+end
